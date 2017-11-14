f918e180dec7367bb291c1b309ceb6ad11b2a882
uds vl <uds@vl-Lets>
2017-11-14 12:56:56 +0900

    ceate repository

---
+++ musician_db/src/main/java/uds/birdmanbros/test/musician_db/App.java
@@ -0,0 +1,27 @@
+package uds.birdmanbros.test.musician_db;
+
+import com.lambdaworks.redis.RedisClient;
+import com.lambdaworks.redis.RedisConnection;
+import com.lambdaworks.redis.api.StatefulRedisConnection;
+import com.lambdaworks.redis.api.sync.RedisCommands;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "alo!" );
+ 
+    	RedisClient redisClient = RedisClient.create("redis://localhost:6379/15");
+    	StatefulRedisConnection<String, String> connection = redisClient.connect();
+    	RedisCommands<String, String> syncCommands = connection.sync();
+
+    	syncCommands.set("lettuce", "Hello, Redis!");
+
+    	connection.close();
+   	redisClient.shutdown();
+    }
+}
